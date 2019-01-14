61e4ab47392d8fc101dfc9be0cbfc4f3b45bb78b
gaurav bansal <bansalgaurav717@gmail.com>
2019-01-14 09:07:38 +0530

    added files and structure

---
+++ Datatypes/src/bytes.java
@@ -0,0 +1,9 @@
+class bytes
+{
+	public static void main(String args[])
+	{
+	System.out.println("Int occupies" + Integer.BYTES +"bytes");
+	System.out.println("Float occupies" + Float.BYTES +"bytes");
+
+	}
+}
---
+++ Datatypes/src/inteqi.java
@@ -0,0 +1,11 @@
+class inteqi
+{
+	public static void main(String args[])
+	{
+	int a;
+	char ch;
+	ch='a';
+	a=ch;
+	System.out.println("integer representation of given character is "+a);
+	}
+}
